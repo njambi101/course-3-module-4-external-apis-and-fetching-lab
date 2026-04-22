af4dd375fb3be93bc22a1a3378da7764c62de9dd
njambi101 <grace.nganga@student.moringaschool.com>
2026-04-22 15:44:18 +0300

    changes

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
-// index.js
-const weatherApi = "https://api.weather.gov/alerts/active?area="
 
-// Your code here!
+const allertd = document.getElementById('alerts-display');
+const errorm = document.getElementById('error-message');
+
+async function fetchAPI(words) {
+  
+    let result; 
+    try {
+       
+        const weatherApi = `https://api.weather.gov/alerts/active?area=${words}`;
+        
+        const response = await fetch(weatherApi);
+        result = await response.json();
+        
+        
+        if (result && result.features) {
+           
+            allertd.innerText = result.features[0]?.id || 'No ID found';
+        }else{
+            allertd.innerText='no active alerts for this area';
+        }
+    } catch (error) {
+        console.error("Fetch error:", error);
+        errorm.innerText = "Failed to load weather data.";
+    }
+}
+
+
+    
+
+
+fetchAPI(); 
+const buttonelem = document.getElementById('button-id'); 
+buttonelem.addEventListener('click', (e) => {
+    const inputVal = document.getElementById('input-id').value; 
+    if (inputVal) {
+        fetchAPI(inputVal);
+}}
+);
+
+
+
+
